a54ae5aa7cb893bc820e7cc27e6a15dcb116245a
Nikolai <42339208+NickOvt@users.noreply.github.com>
2026-03-23 19:44:36 +0200

    add more test strategies

--- avlhashtree/test/avlhashtree_test.go
+++ avlhashtree/test/avlhashtree_test.go
@@ -16,6 +16,25 @@ func appendResult(t *testing.T, options *test_interfaces.BenchmarkOptions) {
 	allResults = append(allResults, test_interfaces.TestWithProfile(t, options, now))
 }
 
+func newInsertOnlyBenchmarkOptions(elementCount int, scenarioName string) *test_interfaces.BenchmarkOptions {
+	return &test_interfaces.BenchmarkOptions{
+		TreeType:       test_interfaces.AVLHASHTREE,
+		ScenarioName:   scenarioName,
+		CPUProfile:     true,
+		ElementCount:   elementCount,
+		SampleSize:     0.01,
+		MeasureInserts: true,
+		BlockSizeBytes: 32,
+		DataSizeBytes:  8,
+	}
+}
+
+func newProofOnlyBenchmarkOptions(buildCount int, sampleSize float32, scenarioName string) *test_interfaces.BenchmarkOptions {
+	options := test_interfaces.NewProofOnlyBenchmarkOptions(test_interfaces.AVLHASHTREE, buildCount, sampleSize)
+	options.ScenarioName = scenarioName
+	return options
+}
+
 func TestMain(m *testing.M) {
 	// Run all tests
 	now = time.Now()
@@ -93,12 +112,92 @@ func TestAVL_25M(t *testing.T) {
 //	appendResult(t, &test_interfaces.BenchmarkOptions{TreeType: test_interfaces.AVLHASHTREE, CPUProfile: true, ElementCount: 100_000_000, SampleSize: 0.01, MeasureInserts: true, IncludeInclusionProof: true, BlockSizeBytes: 32, DataSizeBytes: 8})
 //}
 
-func TestAVL_ProofOnly_5M(t *testing.T) {
-	appendResult(t, test_interfaces.NewProofOnlyBenchmarkOptions(test_interfaces.AVLHASHTREE, 5_000_000, 0.01))
+func TestAVL_InsertOnly_10k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(10_000, "insert_only_build_10k"))
+}
+
+func TestAVL_InsertOnly_50k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(50_000, "insert_only_build_50k"))
+}
+
+func TestAVL_InsertOnly_100k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(100_000, "insert_only_build_100k"))
+}
+
+func TestAVL_InsertOnly_200k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(200_000, "insert_only_build_200k"))
+}
+
+func TestAVL_InsertOnly_300k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(300_000, "insert_only_build_300k"))
+}
+
+func TestAVL_InsertOnly_500k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(500_000, "insert_only_build_500k"))
+}
+
+func TestAVL_InsertOnly_700k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(700_000, "insert_only_build_700k"))
+}
+
+func TestAVL_InsertOnly_900k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(900_000, "insert_only_build_900k"))
+}
+
+func TestAVL_InsertOnly_1M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(1_000_000, "insert_only_build_1m"))
+}
+
+func TestAVL_InsertOnly_2M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(2_000_000, "insert_only_build_2m"))
+}
+
+func TestAVL_InsertOnly_5M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(5_000_000, "insert_only_build_5m"))
+}
+
+func TestAVL_InsertOnly_10M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(10_000_000, "insert_only_build_10m"))
+}
+
+func TestAVL_InsertOnly_25M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(25_000_000, "insert_only_build_25m"))
+}
+
+func TestAVL_ProofOnly_1M_Sample5Pct(t *testing.T) {
+	appendResult(t, newProofOnlyBenchmarkOptions(1_000_000, 0.05, "proof_only_after_1m_build_sample_5pct"))
+}
+
+func TestAVL_ProofOnly_5M_Sample3Pct(t *testing.T) {
+	appendResult(t, newProofOnlyBenchmarkOptions(5_000_000, 0.03, "proof_only_after_5m_build_sample_3pct"))
+}
+
+func TestAVL_ProofOnly_10M_Sample2Pct(t *testing.T) {
+	appendResult(t, newProofOnlyBenchmarkOptions(10_000_000, 0.02, "proof_only_after_10m_build_sample_2pct"))
+}
+
+func TestAVL_1MThenAdd200k_NewElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.AVLHASHTREE, 1_000_000, 200_000))
+}
+
+func TestAVL_2MThenAdd400k_NewElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.AVLHASHTREE, 2_000_000, 400_000))
 }
 
 func TestAVL_5MThenAdd1M_NewElements(t *testing.T) {
 	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.AVLHASHTREE, 5_000_000, 1_000_000))
+}
+
+func TestAVL_10MThenAdd2M_NewElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.AVLHASHTREE, 10_000_000, 2_000_000))
+}
+
+func TestAVL_1MThenReinsert200k_ExistingElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewExistingKeyInsertBenchmarkOptions(test_interfaces.AVLHASHTREE, 1_000_000, 200_000))
+}
+
+func TestAVL_2MThenReinsert400k_ExistingElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewExistingKeyInsertBenchmarkOptions(test_interfaces.AVLHASHTREE, 2_000_000, 400_000))
 }
 
 func TestAVL_5MThenReinsert1M_ExistingElements(t *testing.T) {
--- smt/test/smt_test.go
+++ smt/test/smt_test.go
@@ -16,6 +16,25 @@ func appendResult(t *testing.T, options *test_interfaces.BenchmarkOptions) {
 	allResults = append(allResults, test_interfaces.TestWithProfile(t, options, now))
 }
 
+func newInsertOnlyBenchmarkOptions(elementCount int, scenarioName string) *test_interfaces.BenchmarkOptions {
+	return &test_interfaces.BenchmarkOptions{
+		TreeType:       test_interfaces.SMT,
+		ScenarioName:   scenarioName,
+		CPUProfile:     true,
+		ElementCount:   elementCount,
+		SampleSize:     0.01,
+		MeasureInserts: true,
+		BlockSizeBytes: 32,
+		DataSizeBytes:  8,
+	}
+}
+
+func newProofOnlyBenchmarkOptions(buildCount int, sampleSize float32, scenarioName string) *test_interfaces.BenchmarkOptions {
+	options := test_interfaces.NewProofOnlyBenchmarkOptions(test_interfaces.SMT, buildCount, sampleSize)
+	options.ScenarioName = scenarioName
+	return options
+}
+
 func TestMain(m *testing.M) {
 	// Run all tests
 	now = time.Now()
@@ -88,20 +107,100 @@ func TestSMT_25M(t *testing.T) {
 	appendResult(t, &test_interfaces.BenchmarkOptions{TreeType: test_interfaces.SMT, CPUProfile: true, ElementCount: 25_000_000, SampleSize: 0.01, MeasureInserts: true, IncludeInclusionProof: true, BlockSizeBytes: 32, DataSizeBytes: 8})
 }
 
-//func TestSMT_50M(t *testing.T) {
-//	appendResult(t, &test_interfaces.BenchmarkOptions{TreeType: test_interfaces.SMT, CPUProfile: true, ElementCount: 50_000_000, SampleSize: 0.01, MeasureInserts: true, IncludeInclusionProof: true, BlockSizeBytes: 32, DataSizeBytes: 8})
-//}
+//	func TestSMT_50M(t *testing.T) {
+//		appendResult(t, &test_interfaces.BenchmarkOptions{TreeType: test_interfaces.SMT, CPUProfile: true, ElementCount: 50_000_000, SampleSize: 0.01, MeasureInserts: true, IncludeInclusionProof: true, BlockSizeBytes: 32, DataSizeBytes: 8})
+//	}
 //
-//func TestSMT_100M(t *testing.T) {
-//	appendResult(t, &test_interfaces.BenchmarkOptions{TreeType: test_interfaces.SMT, CPUProfile: true, ElementCount: 100_000_000, SampleSize: 0.01, MeasureInserts: true, IncludeInclusionProof: true, BlockSizeBytes: 32, DataSizeBytes: 8})
-//}
+//	func TestSMT_100M(t *testing.T) {
+//		appendResult(t, &test_interfaces.BenchmarkOptions{TreeType: test_interfaces.SMT, CPUProfile: true, ElementCount: 100_000_000, SampleSize: 0.01, MeasureInserts: true, IncludeInclusionProof: true, BlockSizeBytes: 32, DataSizeBytes: 8})
+//	}
 
-func TestSMT_ProofOnly_5M(t *testing.T) {
-	appendResult(t, test_interfaces.NewProofOnlyBenchmarkOptions(test_interfaces.SMT, 5_000_000, 0.01))
+func TestSMT_InsertOnly_10k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(10_000, "insert_only_build_10k"))
+}
+
+func TestSMT_InsertOnly_50k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(50_000, "insert_only_build_50k"))
+}
+
+func TestSMT_InsertOnly_100k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(100_000, "insert_only_build_100k"))
+}
+
+func TestSMT_InsertOnly_200k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(200_000, "insert_only_build_200k"))
+}
+
+func TestSMT_InsertOnly_300k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(300_000, "insert_only_build_300k"))
+}
+
+func TestSMT_InsertOnly_500k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(500_000, "insert_only_build_500k"))
+}
+
+func TestSMT_InsertOnly_700k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(700_000, "insert_only_build_700k"))
+}
+
+func TestSMT_InsertOnly_900k(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(900_000, "insert_only_build_900k"))
+}
+
+func TestSMT_InsertOnly_1M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(1_000_000, "insert_only_build_1m"))
+}
+
+func TestSMT_InsertOnly_2M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(2_000_000, "insert_only_build_2m"))
+}
+
+func TestSMT_InsertOnly_5M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(5_000_000, "insert_only_build_5m"))
+}
+
+func TestSMT_InsertOnly_10M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(10_000_000, "insert_only_build_10m"))
+}
+
+func TestSMT_InsertOnly_25M(t *testing.T) {
+	appendResult(t, newInsertOnlyBenchmarkOptions(25_000_000, "insert_only_build_25m"))
+}
+
+func TestSMT_ProofOnly_1M_Sample5Pct(t *testing.T) {
+	appendResult(t, newProofOnlyBenchmarkOptions(1_000_000, 0.05, "proof_only_after_1m_build_sample_5pct"))
+}
+
+func TestSMT_ProofOnly_5M_Sample3Pct(t *testing.T) {
+	appendResult(t, newProofOnlyBenchmarkOptions(5_000_000, 0.03, "proof_only_after_5m_build_sample_3pct"))
+}
+
+func TestSMT_ProofOnly_10M_Sample2Pct(t *testing.T) {
+	appendResult(t, newProofOnlyBenchmarkOptions(10_000_000, 0.02, "proof_only_after_10m_build_sample_2pct"))
+}
+
+func TestSMT_1MThenAdd200k_NewElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.SMT, 1_000_000, 200_000))
+}
+
+func TestSMT_2MThenAdd400k_NewElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.SMT, 2_000_000, 400_000))
 }
 
 func TestSMT_5MThenAdd1M_NewElements(t *testing.T) {
 	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.SMT, 5_000_000, 1_000_000))
+}
+
+func TestSMT_10MThenAdd2M_NewElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewPostBuildInsertBenchmarkOptions(test_interfaces.SMT, 10_000_000, 2_000_000))
+}
+
+func TestSMT_1MThenReinsert200k_ExistingElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewExistingKeyInsertBenchmarkOptions(test_interfaces.SMT, 1_000_000, 200_000))
+}
+
+func TestSMT_2MThenReinsert400k_ExistingElements(t *testing.T) {
+	appendResult(t, test_interfaces.NewExistingKeyInsertBenchmarkOptions(test_interfaces.SMT, 2_000_000, 400_000))
 }
 
 func TestSMT_5MThenReinsert1M_ExistingElements(t *testing.T) {
